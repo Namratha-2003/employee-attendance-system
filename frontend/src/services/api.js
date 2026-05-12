14eb4339338e9a908e69a3c1493402ea4c2d3d04
Namratha-2003 <namrathaan0@gmail.com>
2026-05-12 18:09:00 +0530

    update frontend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,4 @@
-const API_BASE = 'http://127.0.0.1:8000/api';
-
+const API_BASE = 'https://employee-attendance-system-vwox.onrender.com/api';
 export function getToken() {
   return localStorage.getItem('token');
 }
