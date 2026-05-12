cfec599dec5f954575801c3e59f61cdbb8cba18e
Namratha-2003 <namrathaan0@gmail.com>
2026-05-12 18:26:20 +0530

    fix login api url

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch('http://127.0.0.1:8000/api/auth/login', {
+      const res = await fetch('https://employee-attendance-system-vwox.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
